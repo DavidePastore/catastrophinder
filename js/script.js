657059bdf3f868548f00295b83eddc49493313d0
Davide Pastore <pasdavide@gmail.com>
2025-08-08 11:00:37 +0000

    Fix Standard Style

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13)
 
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mymap);
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap)
 
 $.getJSON('./data/events.json', function (events) {
   console.log('events', events)
