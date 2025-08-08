4944fd4f149af7446836a6b481eb4a61df576514
Davide Pastore <pasdavide@gmail.com>
2025-08-08 10:52:32 +0000

    Fix variable name

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(mymap);
 // L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 //   maxZoom: 18,
 //   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
